e29a69ed7323ffab400c1a058652bdfacea9210e
pritishg3 <ghate.pritish3@gmail.com>
2022-05-31 12:28:53 +0530

    Added assertions

--- alerter.js
+++ alerter.js
@@ -1,11 +1,17 @@
-let alertFailureCount = 0;
+const {expect} = require('chai');
 
+let alertFailureCount = 0;
+let celciusThreshold = 200;
 function networkAlertStub(celcius) {
     console.log(`Alert! Temperature is ${celcius} degrees`);
     // Return 200 for ok
     // Return 500 for not-ok
     // stub always succeeds and returns 200
-    return 200;
+    if (celciusThreshold >= celcius) {
+            return 200;
+    } else {
+            return 500;
+    }
 }
 
 function alertInCelcius(farenheit) {
@@ -21,6 +27,7 @@ function alertInCelcius(farenheit) {
 }
 
 alertInCelcius(400.5);
+expect(alertFailureCount).equals(1);
 alertInCelcius(303.6);
 console.log(`${alertFailureCount} alerts failed.`);
 console.log('All is well (maybe!)');
--- misaligned.js
+++ misaligned.js
@@ -1,16 +1,30 @@
 const {expect} = require('chai')
+const colorPairMap = new Map();
+const majorColors = ["White", "Red", "Black", "Yellow", "Violet"];
+const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
 
-function print_color_map() {
-    const majorColors = ["White", "Red", "Black", "Yellow", "Violet"];
-    const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
+function updateColorPairMap() {    
     for (let i = 0; i < majorColors.length; i++) {
         for (let j = 0; j < minorColors.length; j++) {
-            console.log(`${i * 5 + j} | ${majorColors[i]} | ${minorColors[j]}`);
+            colorPairMap.set((i * 5 + j), `| ${majorColors[i]} | ${minorColors[j]}`);
+        }
+    }    
+}
+
+function print_color_map() {
+    updateColorPairMap();
+    for (let i = 0; i < majorColors.length; i++) {
+        for (let j = 0; j < minorColors.length; j++) {
+            console.log(i * 5 + j,colorPairMap.get(i * 5 + j));
         }
     }
     return majorColors.length * minorColors.length;
 }
 
 result = print_color_map();
+
 expect(result).equals(25);
+expect(colorPairMap.size).equals(25);
+expect(colorPairMap.get(1)).equals('| White | Blue');
+expect(colorPairMap.get(0)).equals(undefined);
 console.log('All is well (maybe!)');
